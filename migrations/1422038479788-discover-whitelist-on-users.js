dd83861215f776661739cfae842c96985b3f9879
Peter and Roy van de Water <sqrt+peter+roy@octoblu.com>
2015-01-23 11:47:02 -0700

    Add discover whitelist on users

---
+++ migrations/1422038479788-discover-whitelist-on-users.js
@@ -0,0 +1,29 @@
+var async      = require('async');
+var _          = require('lodash');
+var connection = require('../connection');
+
+exports.up = function(success, error) {
+  connection.then(function(db){
+    return db.collection('devices').find({type: 'user'}).each(function(error, device){
+      if(error){ return; }
+      if(!device){ return success(); }
+      db.collection('devices').update({uuid: device.uuid}, {$set: {discoverWhitelist: [device.uuid]}}, _.noop);
+    });
+  }, function(err){
+    if(err){ return error(err); }
+    success();
+  });
+};
+
+exports.down = function(success, error) {
+  connection.then(function(db){
+    return db.collection('devices').find({type: 'user'}).each(function(error, device){
+      if(error){ return; }
+      if(!device){ return success(); }
+      db.collection('devices').update({uuid: device.uuid}, {$unset: {discoverWhitelist: true}}, _.noop);
+    });
+  }, function(err){
+    if(err){ return error(err); }
+    success();
+  });
+};
