cc562cdf7480fe93cabe0d07090144db58c46833
Peter and Roy van de Water <sqrt+peter+roy@octoblu.com>
2015-01-23 11:41:12 -0700

    Add discover whitelist on flows

---
+++ migrations/1422038272689-discover-whitelist-on-flows.js
@@ -0,0 +1,30 @@
+var async = require('async');
+var _ = require('lodash');
+var connection = require('../connection');
+var count = 0;
+
+exports.up = function(success, error) {
+  connection.then(function(db){
+    return db.collection('devices').find({type: 'octoblu:flow'}).each(function(error, device){
+      if(error){ return; }
+      if(!device){ return success(); }
+      db.collection('devices').update({uuid: device.uuid}, {$set: {discoverWhitelist: [device.owner]}}, _.noop);
+    });
+  }, function(err){
+    if(err){ return error(err); }
+    success();
+  });
+};
+
+exports.down = function(success, error) {
+  connection.then(function(db){
+    return db.collection('devices').find({type: 'octoblu:flow'}).each(function(error, device){
+      if(error){ return; }
+      if(!device){ return success(); }
+      db.collection('devices').update({uuid: device.uuid}, {$unset: {discoverWhitelist: true}}, _.noop);
+    });
+  }, function(err){
+    if(err){ return error(err); }
+    success();
+  });
+};
